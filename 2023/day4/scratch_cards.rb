c517a4ea1d9d368b4983df9fed843bfdb66dd241
Nastasia Demont <nastasia@demont.me>
2023-12-05 22:00:31 +0100

    2023 - day4 - double recursion cause why not

--- 2023/day4/scratch_cards.rb
+++ 2023/day4/scratch_cards.rb
@@ -3,7 +3,7 @@ FILE_PATH = './day4/input.txt'.freeze
 @cards_pile = []
 @total_points = 0
 @total_cards = 0
-def calculate_points(card, example)
+def calculate_points(card, current_number)
   if card[:winning_numbers].include?(card[:my_numbers][current_number])
     if @current_points.zero?
       @current_points = 1
@@ -20,8 +20,8 @@ end
 def calculate_winning_numbers(card)
   winning_numbers = 0
 
-  card[:my_numbers].each do |number|
-    winning_numbers += 1 if card[:winning_numbers].include?(number)
+  card[:winning_numbers].each do |number|
+    winning_numbers += 1 if card[:my_numbers].include?(number)
   end
 
   winning_numbers
@@ -34,33 +34,31 @@ def parse_file
     winning_numbers = match[2].split.map(&:to_i)
     my_numbers = match[3].split.map(&:to_i)
 
-    @cards_pile[card_number - 1] = { my_numbers: my_numbers, winning_numbers: winning_numbers }
+    @cards_pile[card_number] = { my_numbers: my_numbers, winning_numbers: winning_numbers }
   end
 end
 
-def count_total_cards(current_card, total_copies, total_cards)
-  while current_card < total_copies
-    new_copies = calculate_winning_numbers(@cards_pile[current_card])
-    if new_copies.positive?
-      total_cards = count_total_cards(current_card + 1, current_card + 1 + new_copies, total_cards + new_copies)
-    end
-    current_card += 1
-  end
+def count_total_cards(current_card, left_copies)
+  new_copies = calculate_winning_numbers(@cards_pile[current_card])
 
-  puts total_cards
-  total_cards
+  count_total_cards(current_card + 1, new_copies - 1) if new_copies.positive?
+  count_total_cards(current_card + 1, left_copies - 1) if left_copies.positive?
+
+  @total_cards += 1
 end
 
 parse_file
 puts @cards_pile
 
-# @cards_pile.each do |card|
-  # puts "card #{card}"
-  # @current_points = 0
-  # calculate_points(card, 0) if card
-  # @total_points += @current_points
-# end
+@cards_pile.each do |card|
+  @current_points = 0
+  calculate_points(card, 0) if card
+  @total_points += @current_points
+end
 
-card_pile_size = @cards_pile.size
-@total_cards = count_total_cards(0, card_pile_size, card_pile_size)
+card_pile_size = @cards_pile.size - 1
+puts "card pile size = #{card_pile_size}"
+@total_cards = count_total_cards(1, card_pile_size - 1)
+
+puts @total_points
 puts @total_cards
